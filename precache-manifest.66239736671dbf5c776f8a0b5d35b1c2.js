7abc1ff68b6f7bf4b57a28037cd1c48abefe7162
Phanindra-Reddy <mpr4070@users.noreply.github.com>
2020-04-22 19:56:52 +0530

    Updates

---
+++ precache-manifest.66239736671dbf5c776f8a0b5d35b1c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b029f7ee91153782e897dcb07ff4c463",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "02f11fb1c0d6529c0fe1",
+    "url": "/portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f6c3a0472d6acd0d0889",
+    "url": "/portfolio/static/css/main.eebaa1c2.chunk.css"
+  },
+  {
+    "revision": "02f11fb1c0d6529c0fe1",
+    "url": "/portfolio/static/js/2.b1654e1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.b1654e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c3a0472d6acd0d0889",
+    "url": "/portfolio/static/js/main.5947ac6c.chunk.js"
+  },
+  {
+    "revision": "a1ac68da6391c638e792",
+    "url": "/portfolio/static/js/runtime-main.2023ab3a.js"
+  },
+  {
+    "revision": "60f218f50ea203124348e5b7ff8e8570",
+    "url": "/portfolio/static/media/home-logo.60f218f5.jpg"
+  }
+]);
